74057fa8de94e1420aa5c3f93eee5cb08f24b170
Neil de Vries <neil.b.devries@gmail.com>
2015-05-08 19:16:10 -0400

    more data being logged and displayed with buttons

--- growauto.ino
+++ growauto.ino
@@ -1,17 +1,27 @@
 #include "Arduino.h"
-#include "DHT.h"
+#include <DHT.h>
 #include <LiquidCrystal.h>
 
 // PIN's being used
 #define DHTPIN 2        // humidity + temperature sensor
 #define LHTPIN A0       // light sensor
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+#define BT1PIN 12       // button 1
+#define BT2PIN 11       // button 2
+LiquidCrystal lcd(7, 8, 3, 4, 5, 6); // LCD's PINs
 
 // the type of humidity sensor we are using
 #define DHTTYPE DHT22   // also, AM2302
 
 // Initialize DHT sensor for normal 16mhz Arduino
 DHT dht(DHTPIN, DHTTYPE);
+
+
+// Global Variables
+// readings
+float minHmdt = 1000; // max and min humidity
+float maxHmdt = 0;
+float minTemp = 1000; // max and min temperature
+float maxTemp = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -34,21 +44,43 @@ void loop() {
     return;
   }
   
-  String LCDstr1;              // first line on LCD;
-  LCDstr1 += h;                // humidity,
-  LCDstr1 += "% ";
-  LCDstr1 += t;                // temperature
-  LCDstr1 += "C ";
+  minHmdt = min(minHmdt, h);  // determine min/max values
+  maxHmdt = max(maxHmdt, h);
+  minTemp = min(minTemp, t);
+  maxTemp = max(maxTemp, t);
+   
+  String LCDstr1;  // first line on LCD;
+  String LCDstr2;  // second line on LCD;
   
-  String LCDstr2;              // second line on LCD;
-  LCDstr2 += l;                // light value,
-  LCDstr2 += " ";
-  LCDstr2 += millis() / 1000;  // number of seconds since reset
+  // display values depending on buttons pressed
+  if (digitalRead(BT1PIN) == HIGH) {  // when button 1 is pushed down, display;
+    LCDstr1 += maxHmdt;               // max humidity,
+    LCDstr1 += "% ";
+    LCDstr1 += maxTemp;               // max temperature,
+    LCDstr1 += "C ";
+    LCDstr2 += minHmdt;               // min humidity,
+    LCDstr2 += "% ";
+    LCDstr2 += minTemp;               // min temperature 
+    LCDstr2 += "C ";
+  } else
+  if (digitalRead(BT2PIN) == HIGH) {  // when button 2 is pushed down, display:
+    LCDstr1 += l;                      // light value,
+    LCDstr2 += millis() / 1000;        // seconds since last reset
+  } else {               // Default display (no buttons pushed);
+    LCDstr1 += h;        // current humidity,
+    LCDstr1 += "% ";
+    LCDstr1 += t;        // current temperature,
+    LCDstr1 += "C ";
+    LCDstr2 += minHmdt;  // min humidity,
+    LCDstr2 += "% ";
+    LCDstr2 += minTemp;  // min temperature 
+    LCDstr2 += "C ";
+  }
   
+  lcd.clear(); // clear LCD 
+  // write to LCD
   lcd.setCursor(0, 0);         // cursor to column 0, line 0
   lcd.print(LCDstr1);           
   lcd.setCursor(0, 1);         // cursor to column 0, line 1
   lcd.print(LCDstr2);  
 }
-
-
